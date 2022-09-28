87da289784bf2cb8357f3350bb09510f803103ae
Yovergrover <114543373+Yovergrover@users.noreply.github.com>
2022-09-28 11:58:06 +1000

    added text about my favourite game

---
+++ Assets/Scripts/WeekTwoActivities.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace YovanStankovic
+{
+    public class WeekTwoActivities : MonoBehaviour
+    {
+        private string favouriteGame = "Super Mario Galaxy 2";
+        private int hoursPlayed = 500;
+        private float costofGame = 80f; 
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("My favourite game is " + favouriteGame + "!!!!!");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
